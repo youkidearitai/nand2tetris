4a31cacda9c4303e2e39e4f1e52fc7a056d0acb4
youkidearitai <youkidearitai@gmail.com>
2015-09-19 15:01:28 +0900

    Codeモジュールのガワだけ作成

---
+++ 06/code.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Code:
+    """Codeモジュール
+
+    Hackのアセンブリ言語のニーモニックをバイナリコードへ変換する
+    """
+
+    def dest(self, mnemonic):
+        """
+        destニーモニックのバイナリコードを返す
+
+        Keyword arguments:
+        mnemonic - ニーモニック(文字列)
+        """
+        pass
+
+    def comp(self, mnemonic):
+        """
+        compニーモニックのバイナリコードを返す
+
+        Keyword arguments:
+        mnemonic - ニーモニック(文字列)
+        """
+        pass
+
+    def jump(self):
+        """
+        jumpニーモニックのバイナリコードを返す
+
+        Keyword arguments:
+        mnemonic - ニーモニック(文字列)
+        """
+        pass
+
