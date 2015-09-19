45119c4ad48d99cae6909c0a6f0bf67ad64fe3ac
youkidearitai <youkidearitai@gmail.com>
2015-09-19 15:28:49 +0900

    3つのモジュールのUnitTestを追加

---
+++ 06/test/testcode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class TestCode(unittest.TestCase):
+    """
+    Codeモジュールのテスト
+    """
+
+    def test_dest(self):
+        pass
+
+    def test_comp(self):
+        pass
+
+    def test_jump(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ 06/test/testparser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class TestParser(unittest.TestCase):
+    """
+    Parserモジュールのテスト
+    """
+
+    def test_hasMoreCommands(self):
+        pass
+
+    def test_advance(self):
+        pass
+
+    def test_commandType(self):
+        pass
+
+    def test_symbol(self):
+        pass
+
+    def test_dest(self):
+        pass
+
+    def test_comp(self):
+        pass
+
+    def test_jump(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
