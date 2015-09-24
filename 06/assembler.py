15c1da8da72d33bdfbffa9382833cc0e6696a21e
youkidearitai <youkidearitai@gmail.com>
2015-09-24 21:07:48 +0900

    シンボルを含まないプログラムのためのアセンブラが完成

--- 06/assembler.py
+++ 06/assembler.py
@@ -3,6 +3,7 @@
 import parser
 import code
 import sys
+import re
 
 if len(sys.argv) < 2:
     print("usage: python assembler.py filename")
@@ -10,6 +11,8 @@ if len(sys.argv) < 2:
 f = open(sys.argv[1], 'r')
 p = parser.Parser(f)
 c = code.Code()
+
+isint = re.compile("^[0-9]$")
 
 output_file_name = sys.argv[1].split(".")[0] + ".hack"
 output_file = open(output_file_name, 'w')
@@ -23,6 +26,8 @@ while True:
         break
 
     command_type = p.commandType()
+    if command_type is None:
+        continue
 
     if command_type == parser.C_COMMAND:
         binary = binary + (0b111 << 13)
@@ -30,7 +35,13 @@ while True:
         binary = binary + (c.dest(p.dest()) << 3)
         binary = binary + c.jump(p.jump())
 
-    output_file.write(format(binary, 'b') + "\n")
+    if command_type == parser.A_COMMAND:
+        symbol = p.symbol()
+        if isint.search(symbol):
+            binary = binary + int(symbol)
+
+    write_line = "{0:0>16}".format(format(binary, 'b'))
+    output_file.write(write_line + "\n")
 
 output_file.close()
 f.close()
