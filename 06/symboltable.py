8ccc84a0c21eeedd45b5c5928423a497d1da9a81
youkidearitai <youkidearitai@gmail.com>
2015-09-24 18:40:25 +0900

    SymbolTableモジュールの作成

--- 06/symboltable.py
+++ 06/symboltable.py
@@ -7,11 +7,38 @@ class SymbolTable:
     Hack命令のシンボルを実際のアドレスに変換する
     """
 
+    # シンボルとアドレスのペア
+    symbol_address = None
+
     def __init__(self):
         """
         空のシンボルテーブルを作成する
         """
-        pass
+        self.symbol_address = {
+            "SP":     0x0000,
+            "LCL":    0x0001,
+            "ARG":    0x0002,
+            "THIS":   0x0003,
+            "THAT":   0x0004,
+            "R0":     0x0000,
+            "R1":     0x0001,
+            "R2":     0x0002,
+            "R3":     0x0003,
+            "R4":     0x0004,
+            "R5":     0x0005,
+            "R6":     0x0006,
+            "R7":     0x0007,
+            "R8":     0x0008,
+            "R9":     0x0009,
+            "R10":    0x000a,
+            "R11":    0x000b,
+            "R12":    0x000c,
+            "R13":    0x000d,
+            "R14":    0x000e,
+            "R15":    0x000f,
+            "SCREEN": 0x4000,
+            "KBD":    0x6000
+        }
 
     def addEntity(self, symbol, address):
         """
@@ -21,7 +48,7 @@ class SymbolTable:
         symbol - 文字列
         address - 整数
         """
-        pass
+        self.symbol_address[symbol] = address
 
     def contains(self, symbol):
         """
@@ -31,7 +58,7 @@ class SymbolTable:
         symbol - 文字列
         """
 
-        return True # 戻り値はブール
+        return symbol in self.symbol_address # 戻り値はブール
 
     def getAddress(self, symbol):
         """
@@ -41,5 +68,5 @@ class SymbolTable:
         symbol - 文字列r
         """
 
-        return 0 # 戻り値は整数
+        return self.symbol_address[symbol] # 戻り値は整数
 
--- 06/testsymboltable.py
+++ 06/testsymboltable.py
@@ -2,20 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import symboltable
 
 class TestSymbolTable(unittest.TestCase):
     """
     SymbolTableモジュールのテスト
     """
 
-    def test_dest(self):
-        pass
+    def setUp(self):
+        self.symboltable = symboltable.SymbolTable()
 
-    def test_comp(self):
-        pass
+    def test_symbol(self):
+        self.symboltable.addEntity("hoge", 1)
+        self.assertTrue(self.symboltable.contains("hoge"))
+        self.assertEqual(self.symboltable.getAddress("hoge"), 1)
 
-    def test_jump(self):
-        pass
+        self.symboltable.addEntity("huga", 2)
+        self.assertTrue(self.symboltable.contains("huga"))
+        self.assertEqual(self.symboltable.getAddress("huga"), 2)
+
+        self.assertFalse(self.symboltable.contains("hello"))
+
+        self.assertTrue(self.symboltable.contains("SP"))
+
+        self.assertTrue(self.symboltable.contains("R10"))
+        self.assertEqual(self.symboltable.getAddress("R10"), 0x000a)
 
 if __name__ == '__main__':
     unittest.main()
