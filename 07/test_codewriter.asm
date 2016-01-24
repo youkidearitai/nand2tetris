9171da5521f78acecba9013d899f57d9a5c430f0
youkidearitai <youkidearitai@gmail.com>
2016-01-24 17:35:32 +0900

    not演算のテストコードの追加

--- 07/test_codewriter.asm
+++ 07/test_codewriter.asm
@@ -10,19 +10,10 @@ A=M // アドレスを256に設定する
 M=D // RAM[256]にconstant 10が入る
 @SP
 M=M+1 // SPレジスタ(RAM[256])に1を追加してDレジスタに退避
-// push constant 4 コマンド
-@4 // 4をpushする
-D=A
-@SP
-A=M // アドレスを256に設定する
-M=D // RAM[256]にconstant 4が入る
-@SP
-M=M+1 // SPレジスタ(RAM[256])に1を追加してDレジスタに退避
-// orコマンド
+// notコマンド
 @SP // popするのでアドレスを1減らす
-M=M-1
 D=M
 A=D // アドレスをRAM[SP]に変更する
 D=M // DレジスタにRAM[SP]の中身を退避させる
 A=A-1 // RAM[SP-1]の中身をみるためにアドレスを減算させる
-M=M|D // RAM[SP] | RAM[SP-1]
+M=!M // RAM[SP] ! RAM[SP-1]
